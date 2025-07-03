ca0917e201ebff946cb53c41568368fba9d602eb
Saanvi Kunisetty <4saanvik@gmail.com>
2025-07-03 08:55:44 -0500

    Merged Occurrence Data with Bioclimatic Varibles at Occurrence Points

--- Analysis/pearson.R
+++ Analysis/pearson.R
@@ -2,5 +2,11 @@ library(terra)
 library(dplyr)
 library(corrplot)
 
-source("c:/Users/4saan/Desktop/Mosquito-HSM/worldclim.R")
-source("c:/Users/4saan/Desktop/Mosquito-HSM/Mosquito_data/GBIF.R")
+#source("c:/Users/4saan/Desktop/Mosquito-HSM/worldclim.R")
+#source("c:/Users/4saan/Desktop/Mosquito-HSM/Mosquito_data/GBIF.R")
+
+bioclim_values <- extract(cropped_stack, mosq_points_vect)
+mosq_env <- cbind(mosq_illinois, bioclim_values[, -1])
+
+print(head(mosq_env[, c("decimalLatitude", "decimalLongitude", 
+                  "BIO1 - Annual Mean Temperature", "BIO12 - Annual Precipitation")]))
