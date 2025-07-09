241e08b19adda6c8482448eb5a912f0093ab1b6b
Saanvi Kunisetty <4saanvik@gmail.com>
2025-07-09 09:30:03 -0500

    Extracted Raster Resolution of Predictor Variables in Degrees

--- Mosquito_data/spatial_thinning.R
+++ Mosquito_data/spatial_thinning.R
@@ -3,7 +3,12 @@ library(dplyr)
 library(spatialrisk)
 library(sf)
 
+cropped_stack <- readRDS("c:/Users/4saan/Desktop/Mosquito-HSM/WorldClim/cropped_stack.rds")
 mosq_points_vect <- readRDS("c:/Users/4saan/Desktop/Mosquito-HSM/mosq_points_vect.rds")
 final_vars <- readRDS("c:/Users/4saan/Desktop/Mosquito-HSM/Analysis/final_vars.rds")
 
 mosq_sf <- st_as_sf(mosq_points_vect, coords = c("decimalLongitude", "decimalLatitude"), crs = 4326)
+
+res_deg <- res(cropped_stack)[1]
+res_km <- res_deg * 111
+cat("Raster resolution detected:", res_km, "km\n")
